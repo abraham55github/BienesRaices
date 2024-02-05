46df9c9986e6674665f8d0dde38cca09414ca59f
abraham55github <155010549+abraham55github@users.noreply.github.com>
2024-02-05 09:28:47 -0500

    Primer commit cambiando de pagina estatica a php

--- index.php
+++ index.php
@@ -1,35 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bienes raices</title>
+<?php
+    $inicio = true;
+    include './includes/templates/header.php'
 
-    <link rel="stylesheet" href="build/css/app.css">
-</head>
-<body>
-    <header class="header inicio" >
-        <div class="contenedor contenido-header">
-            <div class="barra">
-                <a href="/">
-                    <img src="build/img/logo.svg" alt="logotico de bienes raices">
-                </a>
-                <div class="mobile-menu">
-                    <img src="build/img/barras.svg" alt="icono menu responsive">
-                </div>
-                <div class="derecha">
-                    <img class="dark-mode-boton" src="build/img/dark-mode.svg">
-                    <nav class="navegacion">
-                        <a href="nosotros.html">Nosotros</a>
-                        <a href="anuncios.html">Anuncios</a>
-                        <a href="blog.html">Blog</a>
-                        <a href="contacto.html">Contacto</a>
-                    </nav>
-                </div>
-            </div> <!-- barra -->
-            <h1>Venta de casas y departamento Exclusivos de lujos</h1>
-        </div>
-    </header>
+?>
 
     <main class="contenedor">
         <h1>Mas sobre nosotros</h1>
