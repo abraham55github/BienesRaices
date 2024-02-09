d3cf3f03915057ef5eff7dfd294c21ccf43513c0
abraham55github <155010549+abraham55github@users.noreply.github.com>
2024-02-09 13:08:01 -0500

    Final de proyecto bienes raices con codigo spaggeti

--- index.php
+++ index.php
@@ -37,112 +37,21 @@
     </main>
     <section class="seccion contenedor">
         <h2>Casas y Depas en venta</h2>
-        <div class="contenedor-anuncios">
-            <div class="anuncio">
-                <picture>
-                    <source srcset="build/img/anuncio1.webp" type="image/webp">
-                    <source srcset="build/img/anuncio1.jpg" type="image/jpeg">
-                    <img loading="lazy" src="build/img/anuncio1.jpg" alt="anuncio">
-                </picture>
 
-                <div class="contenido-anuncio">
-                    <h3>Casa de lujo en Lago</h3>
-                    <p>Casa en el lago con excelente vista, acabods de lujo a un excelente precio</p>
-                    <p class="precio">$3,000,000</p>
-                    <ul class="iconos-caracteristicas">
-                        <li>
-                            <img class="icono" loading="lazy" src="build/img/icono_wc.svg" alt="icono wc">
-                            <p>3</p>
-                        </li>
-                        <li>
-                            <img class="icono" loading="lazy" src="build/img/icono_dormitorio.svg" alt="icono dormitorio">
-                            <p>3</p>
-                        </li>
-                        <li>
-                            <img class="icono" loading="lazy" src="build/img/icono_estacionamiento.svg" alt="icono estacionamiento">
-                            <p>3</p>
-                        </li>
-                    </ul>
-                    <a class="boton boton-amarillo-block" href="anuncio.html">
-                        Ver propiedad
-                    </a>
-                </div> <!-- Contenido-anuncio -->
-            </div> <!-- anuncio -->
+        <?php 
+            $limite = 3;
+            include 'includes/template/anuncios.php';
+        ?>
 
-            <div class="anuncio">
-                <picture>
-                    <source srcset="build/img/anuncio2.webp" type="image/webp">
-                    <source srcset="build/img/anuncio2.jpg" type="image/jpeg">
-                    <img loading="lazy" src="build/img/anuncio2.jpg" alt="anuncio">
-                </picture>
-
-                <div class="contenido-anuncio">
-                    <h3>Casa terminados de lujo</h3>
-                    <p>Casa en el lago con excelente vista, acabods de lujo a un excelente precio</p>
-                    <p class="precio">$3,000,000</p>
-                    <ul class="iconos-caracteristicas">
-                        <li>
-                            <img class="icono" loading="lazy" src="build/img/icono_wc.svg" alt="icono wc">
-                            <p>3</p>
-                        </li>
-                        <li>
-                            <img class="icono" loading="lazy" src="build/img/icono_dormitorio.svg" alt="icono dormitorio">
-                            <p>3</p>
-                        </li>
-                        <li>
-                            <img class="icono" loading="lazy" src="build/img/icono_estacionamiento.svg" alt="icono estacionamiento">
-                            <p>3</p>
-                        </li>
-                    </ul>
-                    <a class="boton boton-amarillo-block" href="anuncio.html">
-                        Ver propiedad
-                    </a>
-                </div> <!-- Contenido-anuncio -->
-            </div> <!-- anuncio -->
-
-            <div class="anuncio">
-                <picture>
-                    <source srcset="build/img/anuncio3.webp" type="image/webp">
-                    <source srcset="build/img/anuncio3.jpg" type="image/jpeg">
-                    <img loading="lazy" src="build/img/anuncio3.jpg" alt="anuncio">
-                </picture>
-
-                <div class="contenido-anuncio">
-                    <h3>Casa con alberca</h3>
-                    <p>Casa en el lago con excelente vista, acabods de lujo a un excelente precio</p>
-                    <p class="precio">$3,000,000</p>
-                    <ul class="iconos-caracteristicas">
-                        <li>
-                            <img class="icono" loading="lazy" src="build/img/icono_wc.svg" alt="icono wc">
-                            <p>3</p>
-                        </li>
-                        <li>
-                            <img class="icono" loading="lazy" src="build/img/icono_dormitorio.svg" alt="icono dormitorio">
-                            <p>3</p>
-                        </li>
-                        <li>
-                            <img class="icono" loading="lazy" src="build/img/icono_estacionamiento.svg" alt="icono estacionamiento">
-                            <p>3</p>
-                        </li>
-                    </ul>
-                    <a class="boton boton-amarillo-block" href="anuncio.html">
-                        Ver propiedad
-                    </a>
-                </div> <!-- Contenido-anuncio -->
-            </div> <!-- anuncio -->
-
-
-
-        </div> <!-- contenedor-anuncios -->
         <div class="alinear-derecha">
-            <a href="anuncios.html" class="boton-verde">Ver Todas</a>
+            <a href="anuncios.php" class="boton-verde">Ver Todas</a>
         </div>
     </section>
     
     <section class="imagen-contacto">
         <h2>Encuentra la casas de tus sueños</h2>
         <p>Llena el formulario de contacto y un asesor se pondrá en contacto contigo a la brevedad</p>
-        <a href="contacto.html" class="boton-amarillo">Contactanos</a>
+        <a href="contacto.php" class="boton-amarillo">Contactanos</a>
     </section>
 
     <div class="contenedor seccion seccion-inferior">
@@ -159,7 +68,7 @@
                 </div>
 
                 <div class="texto-entrada">
-                    <a href="entrada.html">
+                    <a href="entrada.php">
                         <h4>Terraza en el techo de tu casa</h4>
                         <p class="informacion-meta">Escrito el :<span>20/10/2023</span> por: <span>Admin</span></p>
                         <p>
@@ -181,7 +90,7 @@
                 </div>
 
                 <div class="texto-entrada">
-                    <a href="entrada.html">
+                    <a href="entrada.php">
                         <h4>Guia para decorasion de hogar</h4>
                         <p class="informacion-meta">Escrito el :<span>20/10/2023</span> por: <span>Admin</span></p>
                         <p>
